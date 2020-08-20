83f5d10d724dfec20c6a369b8931e45a727abd74
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-20 19:35:33 +0900

    ユーザー編集機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
+
+  def after_sign_in_path_for(resource)
+    
   end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @user = current_user #User.find(params[:id])
+  end
+end
