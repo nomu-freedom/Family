dfcc5f59ef4cad7c7d20632725acb5e4a61b844a
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-20 19:34:11 +0900

    room_idカラムの除去

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_024721) do
+ActiveRecord::Schema.define(version: 2020_08_20_090730) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.bigint "room_id", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -24,7 +23,6 @@ ActiveRecord::Schema.define(version: 2020_08_20_024721) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["room_id"], name: "index_users_on_room_id"
   end
 
 end
