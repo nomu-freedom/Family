96211ed055042c85975a40297742522946c87c1d
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-24 16:19:06 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: "rooms#show"
-  get 'rooms/show'
   devise_for :users, controllers: {
     registrations: "users/registrations",
     sessions: "users/sessions"
   }
   resources :users, only: [:show]
+  resources :messages, only: [:create]
+  resources :rooms, only: [:show]
 
   devise_scope :user do
     get "sign_in", to: "users/sessions#new"
