da4ea282307bbb5749a420e7eac28bb896619590
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-25 21:39:20 +0900

    コントローラーの編集

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,11 @@
 class RoomsController < ApplicationController
   def show
-    @messages = Message.includes(:user).order(:id)
+    @messages = Message.includes(:user).order(:id).last(100)
     @message = current_user.messages.build
+  end
+
+  def show_additionally
+    last_id = params[:oldest_message_id].to_i - 1
+    @messages = Message.includes(:user).order(:id).where(id: 1..last_id).last(50)
   end
 end
