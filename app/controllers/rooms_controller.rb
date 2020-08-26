672dccfae9a1246b7e26004337078988dae6ddd0
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-08-26 15:56:57 +0900

    ルームに応じたメッセージの表示

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,25 @@
 class RoomsController < ApplicationController
+
+  def index
+    @rooms = Room.all.order(:id)
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room =Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def show
-    @messages = Message.includes(:user).order(:id).last(100)
+    @room = Room.find(params[:id])
+    @messages = @room.messages.includes(:user).order(:id).last(100)
     @message = current_user.messages.build
   end
 
@@ -8,4 +27,10 @@ class RoomsController < ApplicationController
     last_id = params[:oldest_message_id].to_i - 1
     @messages = Message.includes(:user).order(:id).where(id: 1..last_id).last(50)
   end
+
+  private
+
+    def room_params
+      params.require(:room).permit(:name)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,35 @@
 
 user_count = 3
 message_count = 1000
+room_count = 2
 
 ApplicationRecord.transaction do
   # テストユーザーが無ければ作成
   user_count.times do |n|
     User.find_or_create_by!(email: "test#{n + 1}@example.com") do |user|
       user.password = 'password'
-      user.nickname = Faker::JapaneseMedia::OnePiece
+      user.nickname = Faker::JapaneseMedia::OnePiece.character
+    end
+  end
+
+  room_count.times do |n|
+    Room.create! do |room|
+      room.name = Faker::JapaneseMedia::OnePiece.sea
     end
   end
 
   # メッセージを全消去した上で，サンプルメッセージを作成。メッセージを作成したユーザーはランダムに決定する
   Message.destroy_all
   user_ids = User.ids
+  room_ids = Room.ids
   message_list = []
   message_count.times do |n|
     user_id = user_ids.sample
+    room_id = room_ids.sample
     line_count = rand(1..4)
     # Fakerで１〜４行のランダムメッセージを作成
     content = Faker::Lorem.paragraphs(number: line_count).join("\n")
-    message_list << { user_id: user_id, content: content }
+    message_list << { user_id: user_id, content: content, room_id: room_id}
   end
   Message.create!(message_list)
 end
