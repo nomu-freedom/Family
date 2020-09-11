129df59e7b4412716bbf3fb04d634395a9677354
nomu-freedom <68408966+nomu-freedom@users.noreply.github.com>
2020-09-11 12:38:02 +0900

    不要なルーティングの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount ActionCable.server => "/cable"
-  resources :rooms, only: [:index, :new, :show, :create, :update] do
+  resources :rooms, only: [:index, :new, :show, :create] do
     resources :messages, only: [:create]
   end
   root to: "rooms#index"
